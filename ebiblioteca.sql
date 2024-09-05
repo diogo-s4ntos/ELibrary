a70189973d6032f46a73b5b99b9cab0bbaf04280
Diogo Santos <diogomariasantos3@gmail.com>
2024-09-05 23:39:53 +0100

    Database tables code updated

--- ebiblioteca.sql
+++ ebiblioteca.sql
@@ -1,17 +1,17 @@
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100),
-    email VARCHAR(150),
+    username VARCHAR(25),
+    email VARCHAR(50),
     password VARCHAR(19)
 );
 
 CREATE TABLE livros (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    autor VARCHAR(100),
-    titulo VARCHAR(150),
-    descricao VARCHAR(400),
+    autor VARCHAR(50),
+    titulo VARCHAR(100),
+    descricao VARCHAR(2200),
     ano INT,
-    genero VARCHAR(255),
+    genero VARCHAR(450),
     user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
