4e0faa8fc8e7e6d331c8a51c054ef9bf3e50b32a
Diogo Santos <diogomariasantos3@gmail.com>
2024-09-05 23:42:54 +0100

    Conection updated

--- PHP/conection.php
+++ PHP/conection.php
@@ -5,9 +5,4 @@ $username = 'root';
 $password = 'root';
 $database = 'biblioteca';
 
-// $host = 'fdb1028.awardspace.net';	
-// $username = '4524729_biblioteca';
-// $password = 'root6131';
-// $database = '4524729_biblioteca';
-
 $mysqli = new mysqli($host, $username, $password, $database);
